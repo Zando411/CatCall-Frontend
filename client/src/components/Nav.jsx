28a57dccb5096bb9c38dc5127b7fab23452ac68b
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-02-11 08:49:52 -0800

    update nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/outlinedLogo.svg";
-import heroImage from "../assets/heroImage.jpg";
+import profileIcon from "../assets/profile.svg";
 
 export default function Nav() {
-  const avatarImage = heroImage;
+  const avatarImage = profileIcon;
 
   return (
-    <nav className="bg-accent relative flex h-20 items-center justify-center px-8 text-white">
-      <NavLink to="/" end>
+    <nav className="bg-accent absolute top-0 flex h-20 w-full items-center justify-center px-8 text-white">
+      <NavLink to="/dashboard" end>
         <img src={logo} alt="CatCall Logo" className="h-16" />
       </NavLink>
-      <div className="absolute top-4 right-4 flex h-12 w-12 items-center justify-center overflow-hidden rounded-full shadow-md">
-        {avatarImage ? (
-          <img
-            src={avatarImage}
-            alt="Avatar"
-            className="h-full w-full object-cover"
-          />
-        ) : (
-          <span>MZ</span> // Replace "ZZ" with initials or any placeholder text
-        )}
+      <div className="absolute top-4 right-4 flex h-12 w-12 items-center justify-center overflow-hidden rounded-full shadow-md outline-2 outline-white">
+        <img
+          src={avatarImage}
+          alt="Avatar"
+          className="color-white h-2/3 w-2/3 object-cover"
+        />
       </div>
     </nav>
   );
