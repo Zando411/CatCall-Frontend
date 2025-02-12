297915f16d4ff2b082c74e10f27a467af3d14e24
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-02-11 16:00:04 -0800

    update dashboard

---
+++ client/src/components/CatCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CatCard() {
+  return (
+    <div>
+      <h1>Cat Card</h1>
+    </div>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,19 +2,18 @@ import logo from "../assets/logo.svg";
 import Button from "../components/Button";
 import { NavLink } from "react-router-dom";
 import Nav from "../components/Nav";
+import CatSwipe from "../components/CatCard.jsx";
 
 export default function Dashboard() {
   return (
     <>
-      <Nav></Nav>
-      <div className="mx-auto max-w-[1440px] px-8 text-center">
-        <body className="m-0 flex min-h-screen min-w-[320px] items-center justify-center">
-          <div className="relative mx-8 flex w-full items-start justify-around gap-8">
-            <div className="mt-20 w-full">
-              <p className="text-center">aaa</p>
-            </div>
-          </div>
-        </body>
+      <div className="bg-black-cat text-white">
+        <Nav></Nav>
+        <div className="mx-auto max-w-[1440px] px-8 text-center">
+          <body className="flex min-h-screen min-w-[320px] flex-col items-center justify-center p-4 pt-24">
+            <CatSwipe />
+          </body>
+        </div>
       </div>
     </>
   );
