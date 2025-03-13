8ca62fc4cdb5fd12f59deaec8a55fcaaf1c38d78
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-03-12 23:16:34 -0700

    rename catswipe to catcard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.svg";
 import Button from "../components/Button";
 import { NavLink } from "react-router-dom";
 import Nav from "../components/Nav";
-import CatSwipe from "../components/CatCard.jsx";
+import CatCard from "../components/CatCard.jsx";
 
 export default function Dashboard() {
   return (
@@ -11,7 +11,7 @@ export default function Dashboard() {
         <Nav></Nav>
         <div className="mx-auto max-w-[1440px] px-8 text-center">
           <body className="flex min-h-screen min-w-[320px] flex-col items-center justify-center p-4 pt-24">
-            <CatSwipe />
+            <CatCard />
           </body>
         </div>
       </div>
