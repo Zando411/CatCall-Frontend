1aa0f7413ce3a8bfb40d619bf3f683ba5b97724d
Zando411 <123271994+Zando411@users.noreply.github.com>
2025-04-03 13:08:23 -0700

    refactor landing page and add grid

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import heroImage from "../assets/heroImage.jpg";
 import logo from "../assets/logo.svg";
 import Button from "../components/Button";
@@ -7,17 +6,11 @@ import { NavLink } from "react-router-dom";
 export default function Homepage() {
   return (
     <>
-      <div className="mx-auto max-w-[1440px] px-8 text-center">
-        <div className="m-0 flex min-h-screen min-w-[320px] items-center justify-center">
-          <div className="relative mx-8 flex w-full items-start justify-around gap-8">
-            <NavLink to="/auth" end>
-              <Button className="absolute -top-14 right-0" defaults={true}>
-                Sign In
-              </Button>
-            </NavLink>
-
+      <div className="mx-auto max-w-[1440px] text-center">
+        <div className="min-h-screen">
+          <div className="relative grid place-items-center gap-8 px-8 pt-8 lg:grid-cols-12">
             {/* left half of hero */}
-            <div className="mt-20 w-full basis-3/5">
+            <div className="col-span-7">
               <div className="flex w-fit flex-col items-end justify-center gap-2">
                 <NavLink to="/" end>
                   <img src={logo} alt="CatCall Logo" className="" />
@@ -28,13 +21,13 @@ export default function Homepage() {
                 </h2>
                 <NavLink to="/auth" end>
                   <Button className={"primary"} defaults={true}>
-                    Join Now
+                    Sign In
                   </Button>
                 </NavLink>
               </div>
             </div>
             {/* right half of hero */}
-            <div className="w-full basis-2/5">
+            <div className="col-span-5 hidden lg:block">
               <img
                 src={heroImage}
                 alt="Picture of a very handsome cat"
@@ -44,6 +37,7 @@ export default function Homepage() {
           </div>
         </div>
       </div>
+      <div className="bg-mitten-white absolute inset-0 -z-10 bg-[linear-gradient(to_right,#80808030_1px,transparent_1px),linear-gradient(to_bottom,#80808030_1px,transparent_1px)] bg-[size:28px_28px]"></div>
     </>
   );
 }
